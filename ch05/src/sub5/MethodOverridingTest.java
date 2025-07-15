a3e350343e57909d5246811ec0ea975fb31fe7db
sy7521012 <sy7521012@naver.com>
2025-07-15 11:24:41 +0900

    modified MethodOverridingTest.java && add Car,Sedan,Truck

--- ch05/src/sub5/MethodOverridingTest.java
+++ ch05/src/sub5/MethodOverridingTest.java
@@ -19,23 +19,31 @@ class AAA {
 }
 
 class BBB extends AAA {
+	
+	@Override
 	public void method2() {
+		// 부모클래스 method2를 Override하기 때문에 어노테이션 @Override 표시
 		System.out.println("BBB:method2...");
 	}
-	public void method3(int a) {
+	
+	
+	public void method3(int a) { // Overload 메서드
 		System.out.println("BBB:method3...");
 	}
-
 }
 
 class CCC extends BBB {
+	@Override
 	public void method1() {
 		System.out.println("CCC:method1...");
 	}
+	
+	@Override
 	public void method2() {
 		System.out.println("CCC:method2...");
 	}
-	public void method3(int a, int b) {
+	
+	public void method3(int a, int b) { // Overload 메서드
 		System.out.println("CCC:method3...");
 	}
 }
