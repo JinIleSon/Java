59804cf9012a5fe7d2b2ef753beb6f242af1de43
sy7521012 <sy7521012@naver.com>
2025-07-18 17:44:01 +0900

    modified step6/Main6

--- CodingTest/src/step6/Main6.java
+++ CodingTest/src/step6/Main6.java
@@ -11,49 +11,48 @@ public class Main6 {
 		
 		for (int i = 0; i < c.length; i++) {
 				if (i != c.length - 1) {
-					if (c[i] == 'c') {
-						if (c[i+1] == '=' || c[i+1] == '-') {
-							count++;
-							i++;
-						}
+					if (c[i] == 'c' && (c[i+1] == '=' || c[i+1] == '-')) {
+						count++;
+						i++;
 					}
-					if (c[i] == 'd') {
-						if (c[i+1] == 'z') {
-							if (i != c.length - 2) {
+					else if (c[i] == 'd') {
+						if(c[i+1] == 'z') {
+							if (i <= c.length - 2) {
 								if (c[i+2] == '=') {
 									count++;
+									i+=2;
+								}else {
+									count+=2;
 									i+=2;
 								}
 							}
 						}else if(c[i+1] == '-') {
 							count++;
 							i++;
-						}
-					}
-					if (c[i] == 'l') {
-						if (c[i+1] == 'j') {
+						}else {
+							if (c.length == 2)
+								count++;
 							count++;
 							i++;
 						}
 					}
-					if (c[i] == 'n') {
-						if (c[i+1] == 'j') {
-							count++;
-							i++;
-						}
+					else if (c[i] == 'l' && c[i+1] == 'j') {
+						count++;
+						i++;
 					}
-					if (c[i] == 's') {
-						if (c[i+1] == '=') {
-							count++;
-							i++;
-						}
+					else if (c[i] == 'n' && c[i+1] == 'j') {
+						count++;
+						i++;
 					}
-					if (c[i] == 'z') {
-						if (c[i+1] == '=') {
-							count++;
-							i++;
-						}
+					else if (c[i] == 's' && c[i+1] == '=') {
+						count++;
+						i++;
 					}
+					else if (c[i] == 'z' && c[i+1] == '=') {
+						count++;
+						i++;
+					}else 
+						count++;
 			} else {
 				count++;
 			}
