058ac3d6153451748abb324946d54a9709b596ef
sy7521012 <sy7521012@naver.com>
2025-07-18 11:33:52 +0900

    modified step6/Main4

--- CodingTest/src/step6/Main4.java
+++ CodingTest/src/step6/Main4.java
@@ -1,5 +1,20 @@
 package step6;
 
-public class Main4 {
+import java.util.Scanner;
 
+public class Main4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char[] c = sc.next().toCharArray();
+		
+		for (int i = 0; i < (c.length / 2); i++) {
+			if (c[i] != c[c.length - 1 - i]) {
+				System.out.println(0);
+				return ;
+			}
+		}
+		
+		System.out.println(1);
+	}
 }
