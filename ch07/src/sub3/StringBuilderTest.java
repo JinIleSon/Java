8b99f0996a6824173387ef2411df245235182842
sy7521012 <sy7521012@naver.com>
2025-07-21 11:56:32 +0900

    modified StringBuilderTest.java

--- ch07/src/sub3/StringBuilderTest.java
+++ ch07/src/sub3/StringBuilderTest.java
@@ -1,5 +1,31 @@
 package sub3;
 
-public class StringBuilderTest {
 
+/*
+ * 날짜 : 2025/07/21
+ * 이름 : 손진일
+ * 내용 : 자바 StringBuilder 클래스 실습하기
+ */
+
+public class StringBuilderTest {
+	public static void main(String[] args) {
+		
+		// String의 불변성(Immutable)
+		String str = "Java";
+		System.out.println("str 주소 : " + System.identityHashCode(str));
+		
+		str += "Programming";
+		System.out.println("str 주소 : " + System.identityHashCode(str));
+		
+		System.out.println(str);
+		
+		// 빈번한 문자열 변경을 위한 StringBuilder(StringBuffer). Builder나 Buffer는 거의 동일하게 사용.
+		StringBuffer sb = new StringBuffer("Java");
+		System.out.println("sb 주소 : " + System.identityHashCode(str));
+		
+		sb.append("Programming"); // 처음 만들었던 문자열 객체 그대로 사용했음. 메모리 절약!
+		System.out.println("sb 주소 : " + System.identityHashCode(str));
+		
+		System.out.println(sb);
+	}
 }
