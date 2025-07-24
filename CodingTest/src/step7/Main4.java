03cf9a4a39c823559e42b7719d555b3e95219c9f
sy7521012 <sy7521012@naver.com>
2025-07-24 16:51:27 +0900

    modified step7/Main4 && add test6

--- CodingTest/src/step7/Main4.java
+++ CodingTest/src/step7/Main4.java
@@ -1,5 +1,11 @@
 package step7;
 
-public class Main4 {
+import java.util.Scanner;
 
+public class Main4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
 }
