8ed8907606f74b6797558a03c3ce7b03c648f3a9
sy7521012 <sy7521012@naver.com>
2025-07-25 12:48:13 +0900

    add test7/sub03

---
+++ JavaTest/src/test7/sub03/Developer.java
@@ -0,0 +1,24 @@
+package test7.sub03;
+
+public class Developer extends Employee{
+	private String field;
+	
+	public Developer(String name, String birth, String employeeId, String dept, int salary, String field) {
+		super(name, birth, employeeId, dept, salary);
+		this.field = field;
+	}
+
+	public void changeField(String field) {
+		System.out.println("개발 분야 변경");
+		this.field = field;
+	}
+	
+	public void printDeveloperInfo() {
+		System.out.println("이름 : " + name);
+		System.out.println("생년월일 : " + birth);
+		System.out.println("직원 아이디 : " + employeeId);
+		System.out.println("부서 : " + dept);
+		System.out.println("급여 : " + salary);
+		System.out.println("개발분야 : " + field);
+	}
+}
