15e0c28a66169a1af09fd8373dbe8dbbc64649ff
sy7521012 <sy7521012@naver.com>
2025-07-25 12:35:53 +0900

    add sub02

--- JavaTest/src/test7/sub02/Clothes.java
+++ JavaTest/src/test7/sub02/Clothes.java
@@ -1,5 +1,22 @@
 package test7.sub02;
 
-public class Clothes {
+public class Clothes extends Product{
+	private String size;
+	
+	public Clothes(String prodId, String prodName, int price, int quantity, String size) {
+		super(prodId, prodName, price, quantity);
+		this.size = size;
+	}
 
+	@Override
+	public void printProductInfo() {
+		System.out.println("상품코드 : " + prodId);
+		System.out.println("상품이름 : " + prodName);
+		System.out.println("상품가격 : " + price);
+		System.out.println("재고수량 : " + quantity);
+		System.out.println("사이즈 : " + size);
+		
+	}
+	
+	
 }
