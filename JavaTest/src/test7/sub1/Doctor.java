c29bf5d13aba65a99bd24a6ba058504138a13a9a
sy7521012 <sy7521012@naver.com>
2025-07-25 09:54:32 +0900

    add test7 && add ch12

---
+++ JavaTest/src/test7/sub1/Doctor.java
@@ -0,0 +1,29 @@
+package test7.sub1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Doctor {
+	private String name;
+	private String id;
+	private String special;
+	private List<Patient> patients;
+	public Doctor(String name, String id, String special) {
+		this.name = name;
+		this.id = id;
+		this.special = special;
+		this.patients = new ArrayList<Patient>();
+	}
+	
+	public void addPatient(Patient patient) {
+		patients.add(patient);
+	}
+
+	@Override
+	public String toString() {
+		return "의사: " + name + "(ID: " + id + ", 전문분야: " + special + ")" + "\n담당 환자 목록:\n" + patients + "\n";
+	}
+	
+	
+}
+
---
+++ ch12/src/sub2/InsertTest.java
@@ -0,0 +1,14 @@
+package sub2;
+
+/*
+ * 날짜 : 2024/07/25
+ * 이름 : 손진일
+ * 내용 : 자바 Insert 실습하기
+ */
+
+
+public class InsertTest {
+	public static void main(String[] args) {
+		
+	}
+}
