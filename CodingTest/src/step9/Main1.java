c96163439171b3e1716db9f1e80314ce075082c9
thswlsdlf <sy7521012@naver.com>
2025-07-27 12:48:48 +0900

    add step9/Main1

---
+++ CodingTest/src/step9/Main1.java
@@ -0,0 +1,26 @@
+package step9;
+
+import java.util.Scanner;
+
+public class Main1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		while (A != 0 && B != 0) {
+			if (B % A == 0) {
+				System.out.println("factor");
+			}else if(A % B == 0) {
+				System.out.println("multiple");
+			}else {
+				System.out.println("neither");
+			}
+			A = sc.nextInt();
+			B = sc.nextInt();
+			if (A == 0 && B == 0)
+				return ;
+		}
+	}
+}
