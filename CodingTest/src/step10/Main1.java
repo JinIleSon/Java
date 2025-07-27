577e4925d20c2d91aefb7dcbcdea728ffb1586f5
thswlsdlf <sy7521012@naver.com>
2025-07-27 15:31:07 +0900

    modified step10/Main1

--- CodingTest/src/step10/Main1.java
+++ CodingTest/src/step10/Main1.java
@@ -1,5 +1,14 @@
 package step10;
 
-public class Main1 {
+import java.util.Scanner;
 
+public class Main1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A*B);
+	}
 }
