17124437241babd2b74a998a0b24efed7e6d6b41
thswlsdlf <sy7521012@naver.com>
2025-07-28 12:01:02 +0900

    modified step7/Main4

--- CodingTest/src/step7/Main4.java
+++ CodingTest/src/step7/Main4.java
@@ -9,53 +9,22 @@ public class Main4 {
 		int A[][] = new int[100][100];
 		
 		int N = sc.nextInt();
-		int sum = 100*N;
 		
 		for (int i = 0; i < N; i++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			for (int j = y; j < y + 10; j++) {
-				for (int z = x; z < x + 10; z++) {
-					A[j][z] += 1;
+			
+			for (int j = y; j < y+10; j++) {
+				for (int z = x; z < x+10; z++) {
+					A[j][z] = 1;
 				}
 			}
 		}
-		
-		int x_length = 0;
-		int y_length = 0;
-		
-		int i_dummy = 0;
-		int j_dummy = 0;
-		
+		int sum = 0;
 		for (int i = 0; i < 100; i++) {
-			x_length = 0;
-			y_length = 0;
 			for (int j = 0; j < 100; j++) {
-				if (A[i][j] < 2)
-					continue;
-				i_dummy = i;
-				j_dummy = j;
-				while (A[i_dummy][j_dummy] >= 2) {
-					if (i_dummy+1 != 100 && j_dummy+1 != 100 ) {
-						if (A[i_dummy][j_dummy+1] < 2 || A[i_dummy+1][j_dummy] < 2) {
-							break;
-						}
-					}
-					x_length++;
-					j_dummy++;
-				}
-				if(j_dummy >= 1)
-					j_dummy--;
-				while (A[i_dummy][j_dummy] >= 2) {
-					if (i_dummy+1 != 100 && j_dummy+1 != 100 ) {
-						if (A[i_dummy][j_dummy+1] < 2 || A[i_dummy+1][j_dummy] < 2) {
-							break;
-						}
-					}
-					y_length++;
-					i_dummy++;
-				}
-				sum -= (x_length * y_length);
+				if (A[i][j] == 1)
+					sum += 1;
 			}
 		}
 		System.out.println(sum);
