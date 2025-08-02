feeed88ccea96abfa662e042854214850813c39e
sy7521012 <sy7521012@naver.com>
2025-08-02 19:06:44 +0900

    modify step10/Main8

--- CodingTest/src/step10/Main8.java
+++ CodingTest/src/step10/Main8.java
@@ -16,18 +16,21 @@ public class Main8 {
 			sc.close();
 			return;
 		}
+		
 		if (Math.max(Math.max(x, y), z) == x) {
 			if (x < (y + z))
 				System.out.println(x+y+z);
 			else
 				System.out.println(y+z+(y+z-1));
 		}
+		
 		else if (Math.max(Math.max(x, y), z) == y) {
 			if (y < (x + z))
 				System.out.println(x+y+z);
 			else
 				System.out.println(x+z+(x+z-1));
 		}
+		
 		else if (Math.max(Math.max(x, y), z) == z) {
 			if (z < (x+y))
 				System.out.println(x+y+z);
